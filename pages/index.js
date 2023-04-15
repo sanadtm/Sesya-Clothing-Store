d6b0ca315aea88bc68c7e664751e6a4c89c2530d
Sanad Thapa <sanadtm@gmail.com>
2023-04-14 22:53:08 -0700

    add

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import HomePage from '@/components/homepage'
+
+
+export default function Home() {
+  return (
+  <><HomePage /></>
+  )
+}
