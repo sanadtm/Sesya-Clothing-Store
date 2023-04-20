993cd63858e8026bbd59cfae3135cf8e04ad8abb
Sanad Thapa <sanadt@APCONnet.apcon.com>
2023-04-20 10:43:56 -0700

    Created Landing page for Sesya

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import Items from "@/components/item";
+import NavBar from "@/components/NavBar";
 export default function HomePage() {
     return (
         <>
-            <h1 className="pb-16 pt-6 sm:pb-24 text-center">SESYA CLOTHING - Under Construction</h1>
-
+            <NavBar/>
             <Items />
+            <h1 className="pb-16 pt-6 sm:pb-24 text-center">SESYA CLOTHING - Under Construction</h1>
         </>
     );
 }
