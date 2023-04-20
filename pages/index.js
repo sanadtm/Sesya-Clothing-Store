8d5887bf7ca642454fde0f270c4f6248d7ed8fff
Sanad Thapa <sanadt@APCONnet.apcon.com>
2023-04-20 12:03:55 -0700

    Added Title Bar Photo and New Title

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 /** @format */
 
+import Head from "next/head";
 import Items from "@/components/item";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 export default function HomePage() {
 	return (
 		<>
+			<Head>
+				<title>SESYA | Passionate Clothing </title>
+				<link rel="Icon" href="/sesya_insta_logo.png" alt="my personal Logo" />
+			</Head>
+			<title>SESYA | Passionate Clothing </title>
+			<link rel="Icon" href="/sesya_insta_logo.png" alt="my personal Logo" />
 			<NavBar />
 			<Items />
 			<Footer />
