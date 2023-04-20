5cf7418ed3dedb602f3e3b7e3e1a4e23cd0e2f39
Sanad Thapa <sanadt@APCONnet.apcon.com>
2023-04-20 11:55:52 -0700

    Added Footer

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+/** @format */
+
 import Items from "@/components/item";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 export default function HomePage() {
-    return (
-        <>
-            <NavBar/>
-            <Items />
-            <h1 className="pb-16 pt-6 sm:pb-24 text-center">SESYA CLOTHING - Under Construction</h1>
-        </>
-    );
+	return (
+		<>
+			<NavBar />
+			<Items />
+			<h1 className="pb-1 pt-1 sm:pb-1 text-center">SESYA CLOTHING - Under Construction</h1>
+			<Footer />
+		</>
+	);
 }
