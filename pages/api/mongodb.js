a3ff9fe6ec523b12689895816f014028decf418f
Sanad Thapa <sanadt@APCONnet.apcon.com>
2023-04-24 10:51:00 -0700

    Added mongoose connection

--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,22 +1,9 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+/** @format */
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-async function MongoConnect() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
+import mongoose from "mongoose";
+
+export const connectMongo = async () => {
+	mongoose.connect(process.env.MONGODB_URI);
+};
+// import { connectMongo } from '../../../../lib/connectMongo';
+// connectMongo().catch((error) => res.json({ error: 'Connection Failed.... ' }));
